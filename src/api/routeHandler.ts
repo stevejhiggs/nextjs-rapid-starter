18bfd026772300b9dd0407a3ccffbbd8a1dfaaa1
Steve Higgs <steve@veed.io>
2022-11-19 10:28:14 +0000

    Upgrade deps

--- src/api/routeHandler.ts
+++ src/api/routeHandler.ts
@@ -1,9 +1,9 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import nc, { NextConnect } from 'next-connect';
-import { pinoHttp } from 'pino-http';
+import { pinoHttp, Options } from 'pino-http';
 import { pinoConfig } from '@/src/logger/serverLogger';
 
-const logger = pinoHttp(pinoConfig);
+const logger = pinoHttp(pinoConfig as unknown as Options);
 
 export default function handler<T = any>(): NextConnect<NextApiRequest, NextApiResponse<T>> {
   return nc<NextApiRequest, NextApiResponse<T>>().use(logger);
