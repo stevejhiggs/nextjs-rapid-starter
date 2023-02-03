db3dcbfde40749520e70e3ab827842548848cbac
Steve Higgs <steve@veed.io>
2023-02-03 21:29:31 +0000

    use daisy ui to simpllfy styling

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,14 +4,15 @@ import React from 'react';
 interface Props {
   children: React.ReactNode;
   onClick: () => void;
-  variant?: 'default' | 'primary' | 'info' | 'success' | 'warning' | 'danger' | 'dark';
-  size?: 'sm' | 'md' | 'lg';
+  variant?: 'default' | 'primary' | 'secondary' | 'accent' | 'info' | 'success' | 'warning' | 'error' | 'link' | 'ghost';
+  size?: 'xs' | 'sm' | 'md' | 'lg';
   disabled?: boolean;
+  loading?: boolean;
 }
 
-const Button: React.FC<Props> = ({ children, onClick, variant = 'default', size = 'md', disabled, ...rest }) => {
+const Button: React.FC<Props> = ({ children, onClick, variant = 'primary', size = 'md', disabled, ...rest }) => {
   return (
-    <button className={`btn ${variant} ${size}` + (disabled ? ' disabled' : '')} onClick={onClick} disabled={disabled} {...rest}>
+    <button className={`btn btn-${variant} btn-${size}` + (disabled ? ' disabled' : '')} onClick={onClick} disabled={disabled} {...rest}>
       {children}
     </button>
   );
